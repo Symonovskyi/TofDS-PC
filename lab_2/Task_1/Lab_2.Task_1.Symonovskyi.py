7548f4ca287d34c2bfa7aa127688e3f732750001
Sif-on <e.simonovskiy@gmail.com>
2022-10-09 22:45:11 +0300

    Task 1 is solved

--- lab_2/Task_1/Lab_2.Task_1.Symonovskyi.py
+++ lab_2/Task_1/Lab_2.Task_1.Symonovskyi.py
@@ -3,3 +3,21 @@
 викликати функцію, яка виводить в стандартний потік виведення
 послідовність рядків, переданих у функцію у вигляді аргументу.
 '''
+
+import threading
+import time
+
+
+class MyThread(threading.Thread):
+
+    def run(self):
+        print(f'{self.name} started!')
+        time.sleep(.3)
+        print(f'{self.name} finished!')
+
+
+if __name__ == '__main__':
+    for x in range(1, 5):
+        mythread = MyThread(name=f'Thread-{x}')
+        mythread.start()
+        time.sleep(.1)
